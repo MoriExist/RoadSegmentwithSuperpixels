0268756a7cd74251841c09fcc9586e9a7542210c
Mori <147070503+MoriExist@users.noreply.github.com>
2023-12-17 06:45:26 +0800

    Update P1_4_classDataSet.m

--- P1_4_classDataSet.m
+++ P1_4_classDataSet.m
@@ -1,10 +1,10 @@
 close all; clear all; clc;
 
-load .\DataSet\'videoplayback 007.mat'
+load .\DataSet\'videoplayback 138.mat'
 
 outputImage = zeros(size(A), 'like', A);
 
-cName=3;
+cName=7;
 
 for i = 1:size(class{cName},1)
     redIdx = idx{class{cName}(i)};
@@ -15,4 +15,14 @@ for i = 1:size(class{cName},1)
     outputImage(blueIdx) = A(blueIdx);
 end
 
-imshow(outputImage)
+figure;imshow(outputImage)
+Gray=rgb2gray(A);
+roi=~logical(rgb2gray(outputImage));
+Mask=Gray(roi);
+
+RP_R=A(:,:,1); RP_G=A(:,:,2); RP_B=A(:,:,3);
+
+
+RP_R(roi)=Mask;RP_G(roi)=Mask; RP_B(roi)=Mask;
+im_new(:,:,1)=RP_R;im_new(:,:,2)=RP_G; im_new(:,:,3)=RP_B;
+figure;imshow(im_new)
